e66777d571e75887fa30d551f90a1af23cd14522
dluan <luan@realworld.jp>
2015-08-07 19:30:16 +0900

    add app element angularjs

---
+++ angularjs/tutorials/element/element.js
@@ -0,0 +1,28 @@
+/**
+*  Module
+*
+* Description
+*/
+var app = angular.module("app", [])
+
+app.directive("dumbPassword", function () {
+  var validElement = angular.element('<div>{{ model.input }}</div>');
+
+  var link = function (scope) {
+    scope.$watch("model.input", function (value) {
+      if(value === "password") {
+        validElement.toggleClass("alert-box alert");
+      }
+    });
+  };
+
+  return {
+    restrict: "E",
+    replace: true,
+    compile: function (tElem) {
+      tElem.append(validElement);
+
+      return link;
+    }
+  }
+});
